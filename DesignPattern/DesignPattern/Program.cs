e27295c8553a84683e78708bcda6904c7bc59a96
Eric.Zhao <Eric.Zhao@dominos.com.au>
2016-12-28 14:07:20 +1000

    Project set up

---
+++ DesignPattern/DesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This will be the C# practise version of Head First Design Pattern
+            //Motivation for me to do this is practising the code while reading the book of Head First Design Patter
+            //Each exmple code will be in branch, so it can be easily refactor and update after first commit.
+        }
+    }
+}
