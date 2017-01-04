718c5ca21b4aa8a7da315b69a6029b2c895d1e84
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-01-04 16:26:22 +1000

    Template Pattern

---
+++ DesignPattern/TemplatePattern/CaffeineBeverageWithHook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TemplatePattern
+{
+    public abstract class CaffeineBeverageWithHook
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            if (CustomerWantsCondiments())
+            {
+                AddCondiments();
+            }
+        }
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public virtual bool CustomerWantsCondiments()
+        {
+            return true;
+        }
+
+        public abstract void Brew();
+
+        public abstract void AddCondiments();
+    }
+}
---
+++ DesignPattern/TemplatePattern/CoffeeWithHook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace TemplatePattern
+{
+    public class CoffeeWithHook : CaffeineBeverageWithHook
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Dripping Coffeee through filter");
+        }
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("Adding Surger and Milk");
+        }
+
+        public override bool CustomerWantsCondiments()
+        {
+            String anser = GetUserInput();
+            if (anser.ToLower().StartsWith("y"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static string GetUserInput()
+        {
+            string a = null;
+            Console.WriteLine("Would you like milk and tea?");
+            a = Console.ReadLine();
+            return a;
+        }
+    }
+}
